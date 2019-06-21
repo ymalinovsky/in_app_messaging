c8e5faaf70301d14c44303c0b279d82e57fd55df
Yan Malinovsky <yan.malinovsky@gmail.com>
2019-06-21 13:24:49 +0300

    init

---
+++ in_app_messaging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  in_app_messaging
+//
+//  Created by Yan Malinovsky on 6/21/19.
+//  Copyright © 2019 ymalinovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
